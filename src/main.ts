d7090b56d081f06cd59565fce6a91f9d535299b0
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-11-26 19:22:41 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-#!/usr/bin/env node
+#!/usr/bin/env bun
 import readline from "readline";
 import chalk from "chalk";
 import { sendChat } from "./send-chat";
-import { exec } from "child_process";
+import { exec } from "node:child_process";
 
 async function main() {
   const rl = readline.createInterface({
@@ -15,28 +15,44 @@ async function main() {
     userPrompt = await askQuestion(rl, "What do you want to do? ");
   }
 
+  // if (!userPrompt) {
+  //   userPrompt = "rename my git branch";
+  // }
   if (!userPrompt) {
-    userPrompt = "rename my git branch";
+    userPrompt = "list files in this dir";
   }
 
-  console.log(
-    `\n${chalk.underline("user prompt")} ${chalk.green(userPrompt)}\n`
-  );
+  // console.log(
+  //   `\n${chalk.underline("user prompt")} ${chalk.green(userPrompt)}\n`
+  // );
 
   const result = await sendChat(userPrompt);
 
-  if (result?.action) {
-    console.log(
-      `${chalk.underline("action")} ${chalk.yellowBright(result.action)}`
-    );
-  }
-  if (result?.description) {
-    console.log(
-      `${chalk.underline("description")} ${chalk.red(result.description)}\n`
-    );
-  }
+  // if (result?.action) {
+  //   console.log(
+  //     `${chalk.underline("action")} ${chalk.yellowBright(result.action)}`
+  //   );
+  // }
+  // if (result?.description) {
+  //   console.log(
+  //     `${chalk.underline("description")} ${chalk.red(result.description)}\n`
+  //   );
+  // }
 
-  console.log({ result });
+  // console.log({ result });
+
+  if (result.action === "command") {
+    await handleCommand({
+      rl,
+      command: result.command,
+      description: result.description,
+    });
+  }
+  if (result.action === "command_list") {
+    for (const command of result.commands) {
+      await handleCommand({ rl, command, description: result.description });
+    }
+  }
 
   if (result.action === "user_info_required") {
     const values = {};
@@ -45,27 +61,73 @@ async function main() {
       values[item.value] = answer;
     }
 
-    const fullCommand = result.suggested_command.replaceAll(
-      /{(\w+)}/g,
-      (_, match) => {
-        const keyWithBraces = `{${match}}`; // Construct the key with curly braces
-        return values[keyWithBraces] || `{${match}}`; // Replace with value or keep the placeholder if value is not found
+    if (result.suggested_command_list) {
+      for (const command of result.suggested_command_list) {
+        await handleCommand({
+          rl,
+          command,
+          values,
+          description: result.description,
+        });
       }
-    );
-
-    console.log(
-      `\n${chalk.underline("full command")} ${chalk.green(fullCommand)}\n`
-    );
-
-    const answer = await askQuestion(rl, `Run this command? (y/n) `);
-    if (answer === "y") {
-      const proc = Bun.spawn(["echo", "hello"]);
-    } else {
-      console.log("Command aborted.");
+    }
+    if (result.suggested_command) {
+      await handleCommand({
+        rl,
+        command: result.suggested_command,
+        values,
+        description: result.description,
+      });
     }
   }
 }
 main();
+
+async function handleCommand({
+  rl,
+  command,
+  values,
+  description,
+}: {
+  rl: readline.Interface;
+  command: string;
+  values?: Record<string, string>;
+  description?: string;
+}) {
+  let fullCommand = command;
+  if (values) {
+    fullCommand = command.replaceAll(/{(\w+)}/g, (_, match) => {
+      const keyWithBraces = `{${match}}`; // Construct the key with curly braces
+      return values[keyWithBraces] || `{${match}}`; // Replace with value or keep the placeholder if value is not found
+    });
+  }
+
+  console.log(`${chalk.underline("command")}: ${chalk.green(fullCommand)}`);
+  if (description) {
+    console.log(`${chalk.yellow(description)}`);
+  }
+
+  const answer = await askQuestion(rl, `Run this command? (y/n) `);
+  if (answer === "y") {
+    exec(fullCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+
+      console.log(stdout);
+
+      if (stderr) {
+        console.log("STDERR:", stderr);
+      }
+      process.exit(0);
+      return;
+    });
+  } else {
+    console.log("Command aborted.");
+    process.exit(0);
+  }
+}
 
 function askQuestion(rl, query): Promise<string | undefined> {
   return new Promise((resolve, reject) => {
