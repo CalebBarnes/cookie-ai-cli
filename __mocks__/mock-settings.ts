db2355b860a3bb8a49d8d77aff91e169839696b7
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-03-04 20:46:23 -0800

    add test mocks

---
+++ __mocks__/mock-settings.ts
@@ -0,0 +1,14 @@
+import { Settings } from "../src/settings/settings-schema";
+
+export const mockOpenAISettings: Settings = {
+  service: "openai",
+  openai: {
+    key: "missing-key",
+  },
+  model: "gpt-4",
+};
+
+export const mockInvalidSettingsMissingEndpoint: Settings = {
+  service: "custom",
+  model: "gpt-4",
+};
---
+++ __mocks__/test_constants.ts
@@ -0,0 +1,2 @@
+export const TEST_DIR = "./.tmp";
+export const TEST_SETTINGS_PATH = `${TEST_DIR}/test-settings.json`;
