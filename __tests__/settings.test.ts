cd48214661bcf1ec56ad56a88e8362e40b85742a
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-03-09 13:58:39 -0800

    add new settings test for invalid settings

--- __tests__/settings.test.ts
+++ __tests__/settings.test.ts
@@ -1,20 +1,24 @@
 import fs from "node:fs";
-import { describe, it, expect, afterEach, beforeEach } from "vitest";
-import { saveSettings } from "../src/settings/save-settings.js";
-import { errors } from "../src/settings/settings-schema.js";
-import { getSettings } from "../src/settings/get-settings.js";
+import { describe, it, expect, afterEach, beforeEach, vi } from "vitest";
+import { saveSettings } from "../src/settings/save-settings";
+import { errors } from "../src/settings/settings-schema";
+import { getSettings } from "../src/settings/get-settings";
 import {
   mockInvalidSettingsMissingEndpoint,
   mockOpenAISettings,
-} from "../__mocks__/mock-settings.js";
-import { TEST_DIR } from "../__mocks__/test-constants.js";
+} from "../__mocks__/mock-settings";
+import { TEST_DIR } from "../__mocks__/test-constants";
 
 const TEST_SETTINGS_PATH = `${TEST_DIR}/test-settings.json`;
 
 describe("settings", () => {
-  beforeEach(() => {
-    saveSettings(mockOpenAISettings, TEST_SETTINGS_PATH);
-  });
+  const consoleErrorSpy = vi
+    .spyOn(console, "error")
+    .mockImplementation(() => {});
+
+  const mockExit = vi
+    .spyOn(process, "exit")
+    .mockImplementation((code?: number) => undefined as never);
 
   afterEach(() => {
     fs.rmSync(TEST_SETTINGS_PATH, {
@@ -23,14 +27,36 @@ describe("settings", () => {
   });
 
   it("should save and load settings file", () => {
+    expect(() =>
+      saveSettings(mockOpenAISettings, TEST_SETTINGS_PATH)
+    ).not.toThrow();
+
     expect(getSettings(TEST_SETTINGS_PATH)).toEqual(mockOpenAISettings);
   });
 
   it("should throw validation errors and not overwrite existing settings", () => {
+    expect(() =>
+      saveSettings(mockOpenAISettings, TEST_SETTINGS_PATH)
+    ).not.toThrow();
     expect(() => {
       saveSettings(mockInvalidSettingsMissingEndpoint, TEST_SETTINGS_PATH);
     }).toThrow(errors.ENDPOINT_REQUIRED);
 
     expect(getSettings(TEST_SETTINGS_PATH)).toEqual(mockOpenAISettings);
+  });
+
+  it("should fail to get missing settings file and log a message to the user", () => {
+    fs.rmSync(TEST_SETTINGS_PATH, {
+      force: true,
+    });
+
+    expect(() => getSettings(TEST_SETTINGS_PATH)).toThrow();
+    expect(mockExit).toHaveBeenCalledWith(1);
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
+      expect.stringMatching(`Settings file not found: ${TEST_SETTINGS_PATH}
+Run "ai init" to create a new settings file.`)
+    );
+    mockExit.mockRestore();
+    consoleErrorSpy.mockRestore();
   });
 });
