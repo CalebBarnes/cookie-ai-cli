12659d37330422ed50df654f66fbe8e9f99deaa5
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-03-10 23:41:50 -0700

    update mock-settings to match new schema

--- __mocks__/mock-settings.ts
+++ __mocks__/mock-settings.ts
@@ -3,12 +3,14 @@ import { type Settings } from "../src/settings/settings-schema";
 export const mockOpenAISettings: Settings = {
   service: "openai",
   openai: {
+    model: "gpt-4",
     key: "missing-key",
   },
-  model: "gpt-4",
 };
 
 export const mockInvalidSettingsMissingEndpoint: Settings = {
   service: "custom",
-  model: "gpt-4",
+  custom: {
+    model: "gpt-4",
+  },
 };
